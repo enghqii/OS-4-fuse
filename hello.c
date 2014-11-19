71164aa54b7b8630cfab2bd17b3f159c41a8d1b7
ChanWoo Jeong <enghqii@gmail.com>
2014-11-19 22:00:02 +0900

    Touching proc data

--- hello.c
+++ hello.c
@@ -12,84 +12,106 @@
 #include <errno.h>
 #include <fcntl.h>
 
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
 static const char *hello_str = "Hello World!\n";
 static const char *hello_path = "/hello";
 
+struct stat t;
+struct timespec spec;
+
+// getting files' attributes
 static int hello_getattr(const char *path, struct stat *stbuf)
 {
-  int res = 0;
+    int res = 0;
 
-  memset(stbuf, 0, sizeof(struct stat));
-  if(strcmp(path, "/") == 0) {
-    stbuf->st_mode = S_IFDIR | 0755;
-    stbuf->st_nlink = 2;
-  }
-  else if(strcmp(path, hello_path) == 0) {
-    stbuf->st_mode = S_IFREG | 0444;
-    stbuf->st_nlink = 1;
-    stbuf->st_size = strlen(hello_str);
-  }
-  else
-    res = -ENOENT;
+    printf("hello get attr : [%s]\n",path);
 
-  return res;
+    memset(stbuf, 0, sizeof(struct stat));  // clear 'stbuf' ( which is out parameter )
+
+    if(strcmp(path, "/") == 0) {            // if 'path' is root
+        stbuf->st_mode = S_IFDIR | 0755;    // directory 0755
+        stbuf->st_nlink = 2;                // link count is 2
+    }
+    else if(strcmp(path, hello_path) == 0) {
+        stbuf->st_mode = S_IFREG | 0444;    // regular file 0444
+        stbuf->st_nlink = 1;                // link count is 1
+        stbuf->st_size = strlen(hello_str); // setting file's size
+    }
+    else
+        res = -ENOENT;                      // no such a file or dir
+
+    return res;
 }
 
+// read directory
 static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-                         off_t offset, struct fuse_file_info *fi)
+        off_t offset, struct fuse_file_info *fi)
 {
-  (void) offset;
-  (void) fi;
+    // Why casting with void?
+    // it is there to avoid warnings from the compiler because some parameters are unused.
+    (void) offset;
+    (void) fi;
 
-  if(strcmp(path, "/") != 0)
-    return -ENOENT;
+    if(strcmp(path, "/") != 0)              // if path is not root ( we don't need others )
+        return -ENOENT;                     // return error
 
-  filler(buf, ".", NULL, 0);
-  filler(buf, "..", NULL, 0);
-  filler(buf, hello_path + 1, NULL, 0);
+    filler(buf, ".", NULL, 0);              // what is the filter function?
+    filler(buf, "..", NULL, 0);             // create?
+    filler(buf, hello_path + 1, NULL, 0);
 
-  return 0;
+    return 0;
 }
 
+// file open
 static int hello_open(const char *path, struct fuse_file_info *fi)
 {
-  if(strcmp(path, hello_path) != 0)
-    return -ENOENT;
+    if(strcmp(path, hello_path) != 0)       // if not hello path
+        return -ENOENT;                     // error
 
-  if((fi->flags & 3) != O_RDONLY)
-    return -EACCES;
+    if((fi->flags & 3) != O_RDONLY)         // if file is not readonly
+        return -EACCES;                     // error
 
-  return 0;
+    return 0;
 }
 
+// file read
 static int hello_read(const char *path, char *buf, size_t size, off_t offset,
-                      struct fuse_file_info *fi)
+        struct fuse_file_info *fi)
 {
-  size_t len;
-  (void) fi;
-  if(strcmp(path, hello_path) != 0)
-    return -ENOENT;
+    size_t len;
+    (void) fi;                              // useless void casting
 
-  len = strlen(hello_str);
-  if (offset < len) {
-    if (offset + size > len)
-      size = len - offset;
-    memcpy(buf, hello_str + offset, size);
-  } else
-    size = 0;
+    if(strcmp(path, hello_path) != 0)
+        return -ENOENT;
 
-  return size;
+    len = strlen(hello_str);                // store string length of "Hello World"
+    if (offset < len) {
+        if (offset + size > len)
+            size = len - offset;
+        memcpy(buf, hello_str + offset, size);
+    } else
+        size = 0;
+
+    // Read should return exactly the number of bytes requested except on EOF or error,
+    // otherwise the rest of the data will be substituted with zeroes.
+    return size;
 }
 
+// setting operation callback
 static struct fuse_operations hello_oper =
-{
-  .getattr = hello_getattr,
-  .readdir = hello_readdir,
-  .open    = hello_open,
-  .read    = hello_read,
-};
+        {
+                .getattr = hello_getattr,
+                .readdir = hello_readdir,
+                .open    = hello_open,
+                .read    = hello_read,
+        };
 
 int main(int argc, char *argv[])
 {
-  return fuse_main(argc, argv, &hello_oper);
+    // and execute fuse with designated functions.
+    return fuse_main(argc, argv, &hello_oper);
 }
--- pfs.c
+++ pfs.c
@@ -5,46 +5,212 @@
  */
 
 #include <fuse.h>
-#include <stdio.h>
-#include <string.h>
 #include <errno.h>
 #include <fcntl.h>
 
-static int
-pfs_getattr (const char  *path,
-             struct stat *stbuf)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <dirent.h>
+#include <time.h>
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+/* util */
+void get_command_line(int pid, char * out)
 {
-  /* not yet implemented */
-  return -ENOENT;
+    char path[256];
+    FILE* fp;
+    sprintf(path, "/proc/%d/cmdline", pid);
+
+    fp = fopen(path, "r");
+    if(!fp) return;
+
+    fgets(out, 256, fp);
+    fclose(fp);
 }
 
-static int
-pfs_readdir (const char            *path,
-             void                  *buf,
-             fuse_fill_dir_t        filler,
-             off_t                  offset,
-             struct fuse_file_info *fi)
+int get_vmsize(int pid)
 {
-  /* not yet implemented */
-  return -ENOENT;
+    char path[256];
+    char line[256];
+    FILE* fp;
+    int size;
+
+    sprintf(path, "/proc/%d/status", pid);
+
+    fp = fopen(path, "r");
+    if(!fp) return 0;
+
+    while(fgets(line, 256, fp) != NULL)
+    {
+        if(strstr(line, "VmSize:"))
+        {
+            sscanf(line, "VmSize:%d", &size);
+            break;
+        }
+    }
+
+    fclose(fp);
+
+    return size;
 }
 
-static int pfs_unlink (const char *path)
+typedef struct _process_data{
+    int         _pid;
+
+    char        _name[256];
+    int         vm_size;
+    struct stat _stat;
+} process_data;
+
+process_data*   proc_data[32768];
+int             n_proc_data = 0;
+
+process_data*   find_process_data(int pid)
 {
-  /* not yet implemented */
-  return -ENOENT;
+    int i=0;
+    for(i = 0; i < n_proc_data; i++)
+    {
+        if(pid == proc_data[i]->_pid)
+        {
+            return proc_data[i];
+        }
+    }
+    return NULL;
+}
+
+void release_all_process_data()
+{
+    int i=0;
+    for(i = 0; i < n_proc_data; i++)
+    {
+        free(proc_data[i]);
+    }
+    n_proc_data = 0;
+}
+
+void update_all_process_data()
+{
+    static DIR* dir = NULL;
+    struct dirent* entry;	// DIRectory ENTry
+
+    if(dir == NULL)
+        dir = opendir("/proc");
+
+    release_all_process_data();
+
+    while( (entry = readdir(dir)) != NULL ) {
+
+        char 		path[256];
+
+        int         pid = -1;
+        int         vmsize = 0;
+        char        cmdline[256];
+        struct stat fileStat;
+
+        sprintf(path, "/proc/%s", entry->d_name);
+
+        lstat(path, &fileStat);
+        if(!S_ISDIR(fileStat.st_mode))
+            continue;
+
+        pid = atoi(entry->d_name);
+        if( pid < 0 )
+            continue;
+
+        get_command_line(pid, cmdline);
+        vmsize = get_vmsize(pid);
+
+        if(proc_data[n_proc_data] == 0)
+            proc_data[n_proc_data] = (process_data*)malloc(sizeof(process_data));
+
+        proc_data[n_proc_data]->_pid    = pid;
+        proc_data[n_proc_data]->vm_size = vmsize;
+        proc_data[n_proc_data]->_stat   = fileStat;
+        sprintf(proc_data[n_proc_data]->_name, "%d%s", pid, cmdline);
+
+        n_proc_data++;
+    }
+}
+
+void print_all_process_info()
+{
+    int i=0;
+    for(i = 0; i < n_proc_data; i++)
+    {
+        process_data* dat = proc_data[i];
+        printf("[%d] %s\n", dat->_pid, dat->_name);
+    }
+}
+
+static int pfs_getattr(const char  *path, struct stat *stbuf)
+{
+    int res = 0;
+
+    memset(stbuf, 0, sizeof(struct stat));
+
+    if(strcmp(path, "/") == 0) {
+
+        stbuf->st_mode = S_IFDIR | 0755;
+        stbuf->st_nlink = 2;
+    }
+    else if(strcmp(path, "asdf") == 0) {
+
+        stbuf->st_mode = S_IFDIR | 0644;
+        stbuf->st_nlink = 1;
+        stbuf->st_size = strlen("asdf");
+    }
+    else
+        res = -ENOENT;
+
+    return res;
+}
+
+static int pfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
+{
+    int res = 0;
+
+    (void) offset;
+    (void) fi;
+
+    if(strcmp(path, "/") != 0)
+        return -ENOENT;
+
+    // update process data
+
+    filler(buf, ".", NULL, 0);
+    filler(buf, "..", NULL, 0);
+    filler(buf, "asdf", NULL, 0);
+
+    // and filter others
+
+    return res;
+}
+
+// deleting file
+static int pfs_unlink(const char *path)
+{
+    int res = 0;
+    res = -ENOENT;
+    // send SIGKILL
+    return res;
 }
 
 static struct fuse_operations pfs_oper =
 {
-  .getattr  = pfs_getattr,
-  .readdir  = pfs_readdir,
-  .unlink   = pfs_unlink
+    .getattr  = pfs_getattr,
+    .readdir  = pfs_readdir,
+    .unlink   = pfs_unlink
 };
 
-int
-main (int    argc,
-      char **argv)
+
+int main (int argc, char **argv)
 {
-  return fuse_main (argc, argv, &pfs_oper);
+    update_all_process_data();
+    print_all_process_info();
+    //return fuse_main (argc, argv, &pfs_oper);
 }
